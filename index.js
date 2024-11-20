3eeb4f6b32dc9df020fdf8371d394030c685660c
onlybicycle <tskmuratemir@gmail.com>
2024-11-20 20:53:15 +0300

    Güncelleme gel

--- index.js
+++ index.js
@@ -51,18 +51,14 @@ async function updateIndexJs() {
                 console.log(`Güncel bir sürüm bulundu, ${localFileName} dosyası güncelleniyor...`);
                 fs.writeFileSync(localFilePath, remoteCode, 'utf-8');
                 console.log(`${localFileName} başarıyla güncellendi. Uygulama yeniden başlatılıyor...`);
-                setTimeout(() => {
-                    restartApplication();
-                }, 500);
+                setTimeout(() => {}, 500);
             }
         } else {
             console.log('Yerel dosya bulunamadı, yeni bir dosya oluşturuluyor...');
             const newFilePath = path.join(localDir, 'index.js');
             fs.writeFileSync(newFilePath, remoteCode, 'utf-8');
             console.log('index.js başarıyla oluşturuldu. Uygulama yeniden başlatılıyor...');
-            setTimeout(() => {
-                restartApplication();
-            }, 500);
+            setTimeout(() => {}, 500);
         }
     } catch (err) {
         console.error('Güncelleme sırasında bir hata oluştu:', err);
@@ -74,17 +70,6 @@ async function updateIndexJs() {
 
 // Güncelleme kontrolünü ��alıştır
 updateIndexJs();
-
-// Uygulamayı yeniden başlatma fonksiyonu
-function restartApplication() {
-    console.log('Uygulama yeniden başlatılıyor...');
-    try {
-        execSync(`node ${process.argv[1]}`, { stdio: 'inherit' });
-    } catch (err) {
-        console.error('Yeniden başlatma sırasında bir hata oluştu:', err);
-    }
-    process.exit(0); // Mevcut işlemi sonlandır
-}
 
 process.title = 'Lates Tool | v' + version;
 
